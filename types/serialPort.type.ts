ed2e247d90d9d42409b64bd42847695d23202db8
Benjamin <benisin@gmail.com>
2024-11-15 12:18:49 -0800

    changes

--- types/serialPort.type.ts
+++ types/serialPort.type.ts
@@ -4,6 +4,21 @@ import { z } from 'zod';
 
 export type SerialPortName = 'sorter_A' | 'sorter_B' | 'sorter_C' | 'sorter_D' | 'conveyor_jets' | 'hopper_feeder';
 
+export const serialPortNames: { [key: string]: string; [key: number]: string } = {
+  0: 'sorter_A',
+  1: 'sorter_B',
+  2: 'sorter_C',
+  3: 'sorter_D',
+  4: 'conveyor_jets',
+  5: 'hopper_feeder',
+  sorter_A: 'sorter_A',
+  sorter_B: 'sorter_B',
+  sorter_C: 'sorter_C',
+  sorter_D: 'sorter_D',
+  conveyor_jets: 'conveyor_jets',
+  hopper_feeder: 'hopper_feeder',
+} as const;
+
 export const serialPortNamesArray = [
   'sorter_A',
   'sorter_B',
